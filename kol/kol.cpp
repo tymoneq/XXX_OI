20093eda3ffd49f14b3f18bcce66d2325e93312f
TyMooNeEQ <tymontumialis@gmail.com>
2022-10-22 23:58:55 +0200

    działa na 1000

--- kol/kol.cpp
+++ kol/kol.cpp
@@ -1,42 +1,40 @@
 #include <bits/stdc++.h>
 // Tymon Tumialis
 using namespace std;
-// Z 26 77
-int matrix[2000][2000];
-long long czas[2000][2000];
+
+long matrix[2001][2001];
+long czas[2001][2001];
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    int m, p, n, x, y, type;
+    int m, x, y;
+    long p, type, n;
     char oper;
     cin >> m >> p >> n;
     int new_x = 1;
     int new_y = 1;
     // typ konkretnego elementu w snake'u
-    vector<int> snake_type;
+    vector<long> snake_type;
     snake_type.push_back(0);
     // ustawienie wartości na -1
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < m; j++)
-        {
+    for (int i = 0; i < 2001; i++)
+        for (int j = 0; j < 2001; j++)
             matrix[i][j] = -1;
-            czas[i][j] = -1;
-        }
 
     // początkowa pozycja węża
-    czas[1][1] = 0;
+    czas[1][1] = 1;
 
     // wczytanie danych do macierzy
-    for (int i = 0; i < p; i++)
+    for (long i = 0; i < p; i++)
     {
         cin >> x >> y >> type;
         matrix[x][y] = type;
     }
 
-    for (int i = 0; i < n; i++)
+    for (long i = 0; i < n; i++)
     {
 
         cin >> oper;
@@ -44,13 +42,13 @@ int main()
         {
 
             cin >> x >> y;
-            long long time = czas[x][y];
-            if (time == -1)
+
+            long time = czas[x][y];
+            if (time == 0)
                 cout << -1 << "\n";
             else
             {
-                long long current = czas[new_x][new_y];
-                long long size = snake_type.size();
+                long current = czas[new_x][new_y];
                 if (current - time >= snake_type.size())
                     cout << -1 << "\n";
                 else
@@ -70,20 +68,7 @@ int main()
         else if (oper == 'L')
         {
             new_y -= 1;
-            czas[new_x][new_y] = 0;
             czas[new_x][new_y] = czas[new_x][new_y + 1] + 1;
-            if (matrix[new_x][new_y] != -1)
-            {
-                snake_type.push_back(matrix[new_x][new_y]);
-                matrix[new_x][new_y] = -1;
-            }
-        }
-
-        else if (oper == 'D')
-        {
-            new_x += 1;
-            czas[new_x][new_y] = 0;
-            czas[new_x][new_y] = czas[new_x - 1][new_y] + 1;
             if (matrix[new_x][new_y] != -1)
             {
                 snake_type.push_back(matrix[new_x][new_y]);
@@ -93,8 +78,17 @@ int main()
         else if (oper == 'G')
         {
             new_x -= 1;
-            czas[new_x][new_y] = 0;
             czas[new_x][new_y] = czas[new_x + 1][new_y] + 1;
+            if (matrix[new_x][new_y] != -1)
+            {
+                snake_type.push_back(matrix[new_x][new_y]);
+                matrix[new_x][new_y] = -1;
+            }
+        }
+        else if (oper == 'D')
+        {
+            new_x += 1;
+            czas[new_x][new_y] = czas[new_x - 1][new_y] + 1;
             if (matrix[new_x][new_y] != -1)
             {
                 snake_type.push_back(matrix[new_x][new_y]);
