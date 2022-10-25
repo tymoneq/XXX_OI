f12813f29e09ad3e1ecee03825d8c445df535de2
tymoneq <tymontumialis@gmail.com>
2022-10-25 14:50:58 +0200

    poc

--- poc/poc.cpp
+++ poc/poc.cpp
@@ -20,7 +20,7 @@ int main()
     vector<int> Bajtek(n);
     vector<int> Bitek(m);
     vector<pair<vector<poz>, vector<pair<int, int>>>> train(k + 1); // first = Bajtek second = Bitek
-    vector<bool> visited(n);
+    vector<bool> visited(n, 0);
     vector<int> distance_front(n);
     vector<int> distance_back(n);
     // wczytanie danych
