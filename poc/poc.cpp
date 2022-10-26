fadce05c03e465b70d374f6a598c155ce399a557
TyMooNeEQ <tymontumialis@gmail.com>
2022-10-26 18:31:20 +0200

    segmentation fault

--- poc/poc.cpp
+++ poc/poc.cpp
@@ -1,10 +1,92 @@
 #include <bits/stdc++.h>
 // Tymon Tumialis
 using namespace std;
+void binary_b(int search, vector<int> &zbior, int l, int r, int &index)
+{
+    int zakres = (r - l) / 2;
+    int val = zbior[zakres];
 
+
+    if (r == l && zbior.size() > 1)
+    {
+        index = l;
+        return;
+    }
+    else if (r == l && zbior.size() == 1)
+    {
+        if (search >= val)
+        {
+            index = 0;
+            return;
+        }
+        else
+        {
+            index = 2;
+            return;
+        }
+    }
+
+    if (search == val)
+    {
+        index = zakres;
+        return;
+    }
+
+    else if (search > val)
+    {
+        r = zakres;
+        binary_b(search, zbior, l, r, index);
+    }
+    else if (search < val)
+    {
+        l = zakres;
+        binary_b(search, zbior, l, r, index);
+    }
+}
+
+void binary_f(int search, vector<int> &zbior, int l, int r, int &index)
+{
+    int zakres = (r - l) / 2;
+    int val = zbior[zakres];
+    if (r == l && zbior.size() > 1)
+    {
+        index = l;
+        return;
+    }
+    else if (r == l && zbior.size() == 1)
+    {
+        if (search <= val)
+        {
+            index = 0;
+            return;
+        }
+        else
+        {
+            index = 2;
+            return;
+        }
+    }
+
+    if (search == val)
+    {
+        index = zakres;
+        return;
+    }
+
+    else if (search < val)
+    {
+        l = zakres;
+        binary_f(search, zbior, l, r, index);
+    }
+    else if (search > val)
+    {
+        r = zakres;
+        binary_f(search, zbior, l, r, index);
+    }
+}
 int main()
 {
-    // dodaj binary searcha po pierszym zbiorze
+
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
@@ -12,8 +94,8 @@ int main()
     cin >> n >> m >> k;
     vector<int> Bajtek(n);
     vector<int> Bitek(m);
-    // vector<pair<vector<poz>, vector<pair<int, int>>>> train(k + 1); // first = Bajtek second = Bitek
-
+    vector<vector<int>> Bitek_front(k + 1);
+    vector<vector<int>> Bitek_back(k + 1);
     vector<bool> visited(n);
     vector<int> distance_front(n);
     vector<int> distance_back(n);
@@ -42,8 +124,25 @@ int main()
             u--;
         }
     }
-    // binary search
-    // wczytanie do wektora
+    for (int i = 0; i < m; i++)
+    {
+        int val = Bitek[i];
+        Bitek_front[val].push_back(i);
+        Bitek_back[val].push_back(m - 1 - i);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int val = Bajtek[i];
+        int front = distance_front[i];
+        int back = distance_back[i];
+        int index_b = 0, index_f = 0;
+
+        binary_f(front, Bitek_front[val], 0, Bitek_front[val].size() - 1, index_f);
+        binary_b(back, Bitek_back[val], 0, Bitek_back[val].size() - 1, index_b);
+        if (index_b <= index_f)
+            visited[i] = 1;
+    }
 
     for (auto el : visited)
         cout << el << " ";
