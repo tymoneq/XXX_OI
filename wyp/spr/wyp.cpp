35131ad3d12795e7114f7676ade745611e5ef69e
TyMooNeEQ <tymontumialis@gmail.com>
2022-10-30 17:13:58 +0100

    wyp

--- wyp/spr/wyp.cpp
+++ wyp/spr/wyp.cpp
@@ -1,65 +1,57 @@
 #include <bits/stdc++.h>
-// Tymon Tumialis
-using namespace std;
 
+using namespace std;
 struct track
 {
-    float poz_start = 0;
-    float dl = 0;
-    float v = 0;
+    double poz_start = 0;
+    double dl = 0;
+    double v = 0;
 };
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int n, D, wyprzedone = 1;
-    float V_auto, W, M;
+    int n, wyprzedone = 1;
+    double V_auto, W, M, D;
     cin >> n >> D >> W >> M;
+
     vector<track> poz(n + 1);
     V_auto = W / M;
     for (int i = 1; i <= n; i++)
     {
-        float x, d, w, m;
+        double x, d, w, m;
         cin >> x >> d >> w >> m;
-        float v = w / m;
+        double v = w / m;
         track l;
         l.poz_start = x;
         l.dl = d;
         l.v = v;
         poz[i] = l;
     }
-    // liczenie czasu do zrównania się z ostatnim tirem
-    // track last_track = poz.back();
-    // float time = (last_track.poz_start - last_track.dl) / (V_auto - last_track.v);
-    vector<float> contact_time(n + 1);
-    for (int i = 1; i <= n; i++)
+    // znalezienie pierwszej mniejszej prędkości od tyłu
+    int index = 1;
+    for (int i = n; i >= 2; i--)
     {
-        track actual_track = poz[i];
-        float time = (actual_track.poz_start - actual_track.dl) / (V_auto - actual_track.v);
-        contact_time[i] = time;
-    }
-    vector<float> bypass_time(n + 1);
-    for (int i = 1; i <= n; i++)
-    {
-        track actual_track = poz[i];
-        float time = (actual_track.poz_start + D) / (V_auto - actual_track.v);
-        bypass_time[i] = time;
-    }
-    for (int i = 2; i <= n; i++)
-    {
-        float cont_track_time = contact_time[i];
-        float pass_track_time = bypass_time[i - 1];
-        if (cont_track_time < pass_track_time)
-            continue;
-        else
+        if (poz[i].v < poz[i - 1].v)
         {
-            // float poz_first_track_front = pass_track_time * poz[i - 1].v;
-            // float poz_second_track_back = cont_track_time * poz[i].v;
-            // if (D <= poz_first_track_front - poz_second_track_back)
-                wyprzedone += 1;
+            index = i;
+            break;
         }
     }
-    cout << wyprzedone << "\n";
+    // gdy V_1> V_2
+    for (int i = 1; i < index; i++)
+    {
+    }
+    // gdy V_1 <= V_2
+    for (int i = index; i < n; i++)
+    {
+        double time = (poz[i].poz_start + D) / (V_auto - poz[i].v);
+        double pocz_pierwszej = poz[i].poz_start + poz[i].v * time;
+        double kon_drugiej = poz[i + 1].poz_start - poz[i + 1].dl + poz[i + 1].v * time;
+        if (kon_drugiej - pocz_pierwszej >= D)
+            wyprzedone += 1;
+    }
+    cout << wyprzedone;
     return 0;
 }
--- wyp/wyp.cpp
+++ wyp/wyp.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
-// Tymon Tumialis
-using namespace std;
 
+using namespace std;
 struct track
 {
-    float poz_start = 0;
-    float dl = 0;
-    float v = 0;
+    double poz_start = 0;
+    double dl = 0;
+    double v = 0;
 };
 
 int main()
@@ -14,66 +13,54 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int n, wyprzedone = 1;
-    float V_auto, W, M, D;
+    double V_auto, W, M, D;
     cin >> n >> D >> W >> M;
+    vector<pair<int, int>> V_mal; // first = V second = index
     vector<track> poz(n + 1);
     V_auto = W / M;
     for (int i = 1; i <= n; i++)
     {
-        float x, d, w, m;
+        double x, d, w, m;
         cin >> x >> d >> w >> m;
-        float v = w / m;
+        double v = w / m;
         track l;
         l.poz_start = x;
         l.dl = d;
         l.v = v;
         poz[i] = l;
     }
-    // liczenie czasu do zrównania się z ostatnim tirem
-    vector<float> contact_time(n + 1);
-    for (int i = n; i >= 1; i--)
+    // znalezienie pierwszej mniejszej prędkości od tyłu
+    int index = 1;
+    for (int i = n; i >= 2; i--)
     {
-        track actual_track = poz[i];
-        float time = (actual_track.poz_start - actual_track.dl) / (V_auto - actual_track.v);
-        if (i != n)
+        if (poz[i].v < poz[i - 1].v)
         {
-
-            track previous_track = poz[i + 1];
-            if (actual_track.v > previous_track.v)
-            {
-                float time_fix = (previous_track.poz_start - previous_track.dl - actual_track.poz_start) / (actual_track.v - previous_track.v);
-                contact_time[i] =time - time_fix;
-            }
-            else
-                contact_time[i] = time;
-        }
-        else
-            contact_time[i] = time;
-    }
-    vector<float> bypass_time(n + 1);
-    for (int i = 1; i <= n; i++)
-    {
-        track actual_track = poz[i];
-        float time = (actual_track.poz_start + D) / (V_auto - actual_track.v);
-        bypass_time[i] = time;
-    }
-
-    // Musisz sprawdzać v ciężarówki czy nie jest mniejsze
-    // licz czas od tyłu
-    for (int i = 2; i <= n; i++)
-    {
-        float cont_track_time = contact_time[i];
-        float pass_track_time = bypass_time[i - 1];
-        if (cont_track_time < pass_track_time)
-            continue;
-        else
-        {
-            float position_track_1 = poz[i - 1].poz_start + poz[i - 1].v * pass_track_time;
-            float position_track_2 = poz[i].poz_start - poz[i].dl + poz[i].v * cont_track_time;
-            if (position_track_2 - position_track_1 >= D)
-                wyprzedone += 1;
+            index = i;
+            V_mal.push_back(make_pair(poz[i].v, i));
+            break;
         }
     }
-    cout << wyprzedone << "\n";
+    // znalezienie wszystkich najmniejszych V
+    for (int i = index - 1; i > 1; i--)
+    {
+        if (poz[i].v < V_mal.back().first)
+            V_mal.push_back(make_pair(poz[i].v, i));
+    }
+    
+    // gdy V_1> V_2
+    for (int i = 1; i < index; i++)
+    {
+    }
+    
+    // gdy V_1 <= V_2
+    for (int i = index; i < n; i++)
+    {
+        double time = (poz[i].poz_start + D) / (V_auto - poz[i].v);
+        double pocz_pierwszej = poz[i].poz_start + poz[i].v * time;
+        double kon_drugiej = poz[i + 1].poz_start - poz[i + 1].dl + poz[i + 1].v * time;
+        if (kon_drugiej - pocz_pierwszej >= D)
+            wyprzedone += 1;
+    }
+    cout << wyprzedone;
     return 0;
 }
