e856e3331a77176b3de8a287862a34393efba50c
TyMooNeEQ <tymontumialis@gmail.com>
2022-11-01 18:43:50 +0100

    wyp

--- wyp/wyp.cpp
+++ wyp/wyp.cpp
@@ -76,21 +76,33 @@ int main()
         {
             if (Distance_b_tracks[i] == 1)
                 continue;
+            if (i == index - 1)
+            {
+                double time = (poz[i].poz_start + D) / (V_auto - poz[i].v);
+                double pocz_pierwszej = poz[i].poz_start + poz[i].v * time;
+                double kon_drugiej = poz[i + 1].poz_start - poz[i + 1].dl + poz[i + 1].v * time;
+                if (kon_drugiej - pocz_pierwszej >= D)
+                    wyprzedone += 1;
+                continue;
+            }
             double time = (poz[i].poz_start + D) / (V_auto - poz[i].v);
             double pocz_pierwszej = poz[i].poz_start + poz[i].v * time;
             double kon_drugiej = poz[i + 1].poz_start - poz[i + 1].dl + poz[i + 1].v * time;
             if (pocz_pierwszej + D <= poz[i + 1].poz_start - poz[i + 1].dl)
-            {
                 wyprzedone += 1;
-                continue;
-            }
+
             else if (kon_drugiej - pocz_pierwszej >= D)
             {
-                double kon_trzec = poz[i + 2].poz_start - poz[+2].dl + poz[+2].v * time;
-                if (kon_drugiej + poz[i + 1].dl + D <= poz[i + 2].poz_start - poz[i + 2].dl)
-                    wyprzedone += 1;
-
-                else if (kon_trzec - kon_drugiej - poz[i + 1].dl - D >= time * 0.001)
+                double min_V = (kon_drugiej - pocz_pierwszej) / time;
+                float distans = 0;
+                for (int j = i + 2; j < index; j++)
+                {
+                    if (poz[j].v <= min_V)
+                    {
+                        
+                    }
+                }
+                if (kon_drugiej + poz[i + 1].dl <= poz[i + 2].poz_start - poz[i + 2].dl)
                     wyprzedone += 1;
             }
         }
