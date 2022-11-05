23483748bad3258d6cd3a725026be04091d7ba4d
TyMooNeEQ <tymontumialis@gmail.com>
2022-11-05 19:05:08 +0100

    zbo

--- zbo/spraw.cpp
+++ zbo/spraw.cpp
@@ -1,25 +1,29 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int main()
+int main(int argc, char **argv)
 {
     ifstream plik, plik_2;
     string test, test_2;
-    cout << " Podaj numer testu: ";
-    cin >> test;
-    cout << "\n Podaj nazwę swojego wyjścia: ";
-    cin >> test_2;
-    test += ".out";
-    plik.open(test);
+
+    // cin >> argv[1];
+    // cout << argv[1] << endl;
+    test_2 = "wynik.txt";
+    plik.open(argv[1]);
     plik_2.open(test_2);
-    int a, b;
+    int a, b, counter = 1;
+    bool error = false;
     while (plik >> a && plik_2 >> b)
     {
         if (!(a == b))
         {
-            cout << "Error in line " << a;
+            cout << "Error in line " << counter << " " << argv[1] << "\n";
+            error = true;
         }
+        counter += 1;
     }
+    if (!error)
+        cout << "1000%\n";
 
     return 0;
 }
--- zbo/zbo.cpp
+++ zbo/zbo.cpp
@@ -1,51 +1,36 @@
 #include <bits/stdc++.h>
-// Tymon Tumialis
-using namespace std;
 
+using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int n, k, a, b, c, max_w, min_w;
-
+    int n, k;
     cin >> n >> k;
-    // vector przechowuje wartość do głównego zamku i wszystki wioski na drodze
-    vector<pair<int, int>> G(n + 1);
-    for (int i = 1; i < n; i++)
+    vector<int> G(n + 1);
+    for (int i = 0; i < n; i++)
     {
+        int a, b, c;
         cin >> a >> b >> c;
-        max_w = max(a, b);
-        min_w = min(a, b);
-        G[max_w].first = G[min_w].first + c;
-        G[max_w].second = min_w;
+        int maxi = max(a, b);
+        int mini = min(a, b);
+        G[maxi] = c + G[mini];
     }
-
-    int osada, wynik = 0, current_city;
-    // tworzymy wektor gdzie będą zapisywane wszystkie miasta
-    vector<int> krol;
-    // for (int i = 0; i < k; i++)
-    // {
-    //     cin >> osada;
-    //     if (i == 0)
-
-    //         wynik += G[osada].first * 2;
-
-    //     else
-    //     {
-    //         wynik += 2 * G[osada].first;
-    //         // sprawdzamy jaki jest najbliższy punkt między miastem z zapytaniem i innymi miastami w vectorze krol
-    //         for (int j = 0; j < krol.size(); j++)
-    //         {
-    //             current_city = krol[j];
-    //             vector<int> v1(krol.size());
-
-    //             set_intersection(G[max(current_city, osada)].second.begin(), G[max(current_city, osada)].second.end(), G[min(current_city, osada)].second.begin(), G[min(current_city, osada)].second.end(), v1.begin());
-    //             wynik += (G[max(current_city, osada)].first - G[v1.back()].first + G[min(current_city, osada)].first - G[v1.back()].first) * 2;
-    //         }
-    //     }
-    //     krol.push_back(osada);
-    //     cout << wynik << "\n";
-    // }
+    int a, maxi, mini;
+    long long wynik = 0;
+    vector<int> Z(k + 1);
+    Z[0] = 0;
+    for (int i = 0; i < k; i++)
+    {
+        cin >> a;
+        for (auto el : Z)
+        {
+            maxi = max(a, el);
+            mini = min(a, el);
+            wynik += 2 * (G[maxi] - G[mini]);
+        }
+        cout << wynik << "\n";
+    }
     return 0;
 }
