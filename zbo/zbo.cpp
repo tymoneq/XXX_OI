7bed9830a2917141fd1b0e1051dfdd343ee34e76
TyMooNeEQ <tymontumialis@gmail.com>
2022-11-06 11:17:51 +0100

    zbo

--- zbo/zbo.cpp
+++ zbo/zbo.cpp
@@ -8,28 +8,27 @@ int main()
     cout.tie(0);
     int n, k;
     cin >> n >> k;
-    vector<int> G(n + 1);
-    for (int i = 0; i < n; i++)
+    vector<vector<pair<int, int>>> G(n + 1);
+    for (int i = 1; i < n; i++)
     {
         int a, b, c;
         cin >> a >> b >> c;
-        int maxi = max(a, b);
-        int mini = min(a, b);
-        G[maxi] = c + G[mini];
+        G[a].push_back(make_pair(b, c));
+        G[b].push_back(make_pair(a, c));
     }
     int a, maxi, mini;
-    long long wynik = 0;
-    vector<int> Z(k + 1);
-    Z[0] = 0;
+    unsigned long long wynik = 0;
+    vector<int> Zamki;
+    Zamki.push_back(0);
     for (int i = 0; i < k; i++)
     {
         cin >> a;
-        for (auto el : Z)
+        for (auto el : Zamki)
         {
             maxi = max(a, el);
             mini = min(a, el);
-            wynik += 2 * (G[maxi] - G[mini]);
         }
+        Zamki.push_back(a);
         cout << wynik << "\n";
     }
     return 0;
