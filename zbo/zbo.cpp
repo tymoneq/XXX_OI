15d16f7aec649e2c8866dac7df535f4f0ec56061
TyMooNeEQ <tymontumialis@gmail.com>
2022-11-18 17:14:10 +0100

    już jest dobrze

--- zbo/zbo.cpp
+++ zbo/zbo.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 // Tymon Tumialis
 using namespace std;
+struct edge_points
+{
+    int castle = 0;
+    unsigned long long value = 0;
+};
+inline bool sorto(int a, int b) { return a < b; }
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -8,7 +14,7 @@ int main()
     cout.tie(0);
     int n, k;
     cin >> n >> k;
-    vector<vector<pair<int, int>>> G(n + 1);
+    vector<vector<pair<int, int>>> G(n + 1); // first docelowy second droga
     for (int i = 1; i < n; i++)
     {
         int a, b, c;
@@ -16,19 +22,55 @@ int main()
         G[a].push_back(make_pair(b, c));
         G[b].push_back(make_pair(a, c));
     }
-    int a, maxi, mini;
+    int a;
     unsigned long long wynik = 0;
-    vector<int> Zamki;
-    Zamki.push_back(1);
-    for (int i = 0; i < k; i++)
+    // if (n * k <= 1000000) // 1 podzadanie
+    // {
+    //     vector<int> Zamki;
+    //     Zamki.push_back(1);
+    //     for (int i = 0; i < k; i++)
+    //     {
+    //         cin >> a;
+    //         // bfs
+    //         vector<bool> Visited(n + 1);
+    //         vector<long long> distance(n + 1);
+    //         queue<int> q;
+    //         q.push(a);
+    //         Visited[a] = true;
+    //         while (!q.empty())
+    //         {
+    //             int c = q.front();
+    //             q.pop();
+    //             for (auto b : G[c])
+    //             {
+    //                 if (!Visited[b.first])
+    //                 {
+    //                     distance[b.first] = distance[c] + b.second;
+    //                     Visited[b.first] = true;
+    //                     q.push(b.first);
+    //                 }
+    //             }
+    //         }
+    //         for (int castle : Zamki)
+    //         {
+    //             wynik += 2 * distance[castle];
+    //         }
+    //         Zamki.push_back(a);
+    //         cout << wynik << "\n";
+    //     }
+    // }
+    // else // drugie podzadanie
     {
-        cin >> a;
         // bfs
         vector<bool> Visited(n + 1);
-        vector<long long> distance(n + 1);
+        vector<int> Dist(n + 1, 0);
+        vector<int> Zamki_sort;
+        edge_points max_val_up;
+        edge_points min_val_down;
+        int maxi, mini;
         queue<int> q;
-        q.push(a);
-        Visited[a] = true;
+        q.push(1);
+        Visited[1] = true;
         while (!q.empty())
         {
             int c = q.front();
@@ -37,18 +79,78 @@ int main()
             {
                 if (!Visited[b.first])
                 {
-                    distance[b.first] = distance[c] + b.second;
+                    Dist[b.first] = Dist[c] + b.second;
                     Visited[b.first] = true;
                     q.push(b.first);
                 }
             }
         }
-        for (int castle : Zamki)
+
+        for (int i = 0; i < k; i++)
         {
-            wynik += 2 * distance[castle];
+            cin >> a;
+            if (i == 0)
+            {
+                wynik += Dist[a];
+                max_val_up.castle = a;
+                max_val_up.value = 0;
+                min_val_down.castle = a;
+                min_val_down.value = 0;
+                Zamki_sort.push_back(a);
+            }
+            else if (i == 1)
+            {
+                maxi = max(Zamki_sort[0], a);
+                mini = min(Zamki_sort[0], a);
+                wynik += Dist[a] + Dist[maxi] - Dist[mini];
+                max_val_up.castle = maxi;
+                max_val_up.value = Dist[maxi] - Dist[mini];
+                min_val_down.castle = mini;
+                min_val_down.value = Dist[maxi] - Dist[mini];
+                Zamki_sort.push_back(a);
+            }
+            else
+            {
+                sort(Zamki_sort.begin(), Zamki_sort.end(), sorto);
+                if (a < Zamki_sort[0])
+                {
+                    wynik += Dist[a] + min_val_down.value + (Dist[min_val_down.castle] - Dist[a]) * Zamki_sort.size();
+                    min_val_down.value += (Dist[min_val_down.castle] - Dist[a]) * Zamki_sort.size();
+                    min_val_down.castle = a;
+                    max_val_up.value += Dist[max_val_up.castle] - Dist[a];
+                }
+                else if (a < Zamki_sort[1] && a > Zamki_sort[0])
+                {
+                    min_val_down.value += Dist[a] - Dist[min_val_down.castle];
+                    max_val_up.value += Dist[max_val_up.castle] - Dist[a];
+                    wynik += Dist[a] + min_val_down.value - (Dist[a] - Dist[min_val_down.castle]) * (Zamki_sort.size() - 1);
+                }
+                else if (a > Zamki_sort[Zamki_sort.size() - 2] && a < Zamki_sort[Zamki_sort.size() - 1])
+                {
+                    min_val_down.value += Dist[a] - Dist[min_val_down.castle];
+                    max_val_up.value += Dist[max_val_up.castle] - Dist[a];
+                    wynik += Dist[a] + max_val_up.value - (Dist[max_val_up.castle] - Dist[a]) * (Zamki_sort.size() - 1);
+                }
+                else if (a > Zamki_sort[Zamki_sort.size() - 1])
+                {
+                    wynik += Dist[a] + max_val_up.value + (Dist[a] - Dist[max_val_up.castle]) * Zamki_sort.size();
+                    max_val_up.value += (Dist[a] - Dist[max_val_up.castle]) * Zamki_sort.size();
+                    max_val_up.castle = a;
+                    min_val_down.value += Dist[a] - Dist[min_val_down.castle];
+                }
+                else
+                {
+                    wynik += Dist[a];
+                    for (int el : Zamki_sort)
+                        wynik += abs(Dist[a] - Dist[el]);
+                    max_val_up.value += Dist[max_val_up.castle] - Dist[a];
+                    min_val_down.value += Dist[a] - Dist[min_val_down.castle];
+                }
+                Zamki_sort.push_back(a);
+            }
+
+            cout << wynik * 2 << "\n";
         }
-        Zamki.push_back(a);
-        cout << wynik << "\n";
     }
     return 0;
 }
